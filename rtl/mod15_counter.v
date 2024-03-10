e4a3a8db2221920cea6679c703bb4234adeda1bd
A M Basavakiran <91108699+Basavakirana@users.noreply.github.com>
2024-03-10 21:35:44 +0530

    Create mod15_counter.v

---
+++ rtl/mod15_counter.v
@@ -0,0 +1,18 @@
+module mod15_counter (clk,rst,mode,load,data,data_out);
+input clk,rst,mode,load;
+input [3:0]data;
+output reg [3:0] data_out;
+always@(posedge clk)
+begin
+        if(rst)
+                data_out<=0;
+        else if(load)
+                data_out<=data;
+        else if(data_out==4'd14)
+                data_out<=0;
+        else if(mode==1)
+                data_out<=data_out +1'b1;
+        else
+                data_out<=data_out -1'b1;
+end
+endmodule
